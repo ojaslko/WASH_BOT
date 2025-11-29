0f587b77ec8fdb33a5da377859453477be919e6e
ojaslko <ojassaxena0@gmail.com>
2025-11-29 18:44:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -117,7 +117,8 @@ async def whatsapp_webhook(data: Dict):
 # ----------------------------------------------------
 # Local Development Runner (ignored in cloud)
 # ----------------------------------------------------
-#if __name__ == "__main__":
-    #import uvicorn
-    #uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+
 
